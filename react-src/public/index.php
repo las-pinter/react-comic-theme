14522b7e5ad68a5a559ab307f10719df1ffc75db
Las Pinter <plgisli@gmail.com>
2024-02-12 23:44:12 +0200

    impl: Refactor, big one
    Removed wptheme npm package, now the theme is fully operating
    from the create-react-app package.
    
    It was made because of the unmaintained wptheme project and
    the future dependencies are not compatible with it.

--- react-src/public/index.php
+++ react-src/public/index.php
@@ -1,28 +1,4 @@
 <?php
 $TEMPLATE_PATH = parse_url(get_template_directory_uri(), PHP_URL_PATH);
+include('index.html'); // A major sacrilege... I know... But fuck it, it works. It is good until I find something else.
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content="<?php echo get_bloginfo('description'); ?>"
-    />
-    <link rel="apple-touch-icon" href="<?php echo $TEMPLATE_PATH; ?>/logo192.png" />
-    <link rel="manifest" href="<?php echo $TEMPLATE_PATH; ?>/manifest.json" />
-    <title><?php echo get_bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
-</head>
-
-<body>
-    <div id="root"></div>
-</body>
-
-<?php wp_footer(); ?>
-
-</html>
