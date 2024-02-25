d6aa151653a0d97bf1dc6531382a94bd3cab0e9e
Las Pinter <plgisli@gmail.com>
2024-02-25 21:01:16 +0200

    Add sleeper function in RestHandler for testing

--- react-src/src/rest/RestHandler.tsx
+++ react-src/src/rest/RestHandler.tsx
@@ -1,8 +1,14 @@
-import Axios from 'axios';
+import Axios, { AxiosResponse } from 'axios';
 
 module RestHandler {
+    const sleeper = (ms: number) => {
+        return function(x:any) {
+          return new Promise<AxiosResponse<any, any>>(resolve => setTimeout(() => resolve(x), ms));
+        };
+      }
+
     export const get = (url: string) => {
-        return Axios.get(url);
+        return Axios.get(url).then(sleeper(0));
     }
 }
 
