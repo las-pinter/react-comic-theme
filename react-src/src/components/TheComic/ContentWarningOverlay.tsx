262b623f0e30f33f9f1211c4aeef88a78a0d100c
Las Pinter <plgisli@gmail.com>
2024-09-19 01:29:33 +0300

    Modify the content warning printout

--- react-src/src/components/TheComic/ContentWarningOverlay.tsx
+++ react-src/src/components/TheComic/ContentWarningOverlay.tsx
@@ -15,8 +15,9 @@ const ContentWarningOverlay = () => {
             :
             <div className="content-warning-overlay">
                 <div className="content-warning-text">
-                    <p>The content of this comic page is for adults.</p>
-                    <p>Press the button, if you are fine with that.</p>
+                    <p>The content of this comic page is not suitable for minors.</p>
+                    <p>If you are a minor, please fetch a parent or guardian to help you.</p>
+                    <p>Press the button, if you have already reached adulthood.</p>
                 </div>
                 <div className="content-warning-buttons">
                     <button
@@ -24,7 +25,7 @@ const ContentWarningOverlay = () => {
                             setCookie('mature_reader', true);
                         }}
                     >
-                        I'm fine with that
+                        I'm already 18+, let me see the content.
                     </button>
                 </div>
             </div>
